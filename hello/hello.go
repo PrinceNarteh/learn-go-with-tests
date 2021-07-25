f75b6206419581afaf33a560c8fb395f376bfd93
Prince Narteh <prinarteh@gmail.com>
2021-07-25 03:29:03 +0000

    initial commit

---
+++ hello/hello.go
@@ -0,0 +1,29 @@
+package hello
+
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	englishHelloPrefix = "Hello, "
+	frenchHelloPrefix  = "Bonjour, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"learning/hello"
+)
+
+func main() {
+	fmt.Println(hello.Hello("Prince", ""))
+}
